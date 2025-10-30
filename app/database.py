da8d46f483c3c46708f380cbec2d45c4a4935353
anisimovamaria5 <144049821+anisimovamaria5@users.noreply.github.com>
2025-10-30 15:27:06 +0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -6,9 +6,9 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 # DATABASE_URL = settings.get_db_url()
 DATABASE_URL = "sqlite+aiosqlite:///test_dks.db"
 
-engine = create_async_engine(url=DATABASE_URL, echo=True)  # движок SQLAlchemy
+engine = create_async_engine(url=DATABASE_URL, echo=True) 
 
-async_session_maker = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)  # объект генерирующий сессии подключения к БД
+async_session_maker = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession) 
 
 class Base(DeclarativeBase):
     pass
